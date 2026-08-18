65458f29ff82c3421a2f30bc33697daf59b4252e
Kevin Wilhelm <77025567+Kevinwlhlm3@users.noreply.github.com>
2026-08-18 14:23:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,11 @@ const CACHE = "fischindex-v1";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then((c) =>
+      Promise.allSettled(ASSETS.map((a) => c.add(a)))
+    )
+  );
   self.skipWaiting();
 });
 
